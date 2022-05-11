8526d34310698ab52cb5cd800269a01c022c47ce
Miquel Plana <miquelplanab@gmail.com>
2022-05-11 20:47:23 +0200

    config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      blue: "#55d6f7",
+      red: "#f75870",
+      purple: "#d53b9a",
+      green: "#5dfa80",
+      yellow: "#f4f670",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
